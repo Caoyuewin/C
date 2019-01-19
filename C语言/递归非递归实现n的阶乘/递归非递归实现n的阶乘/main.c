b07a6c0b2ec2ebb15426fa55bd6244f1cbe3acc8
Caoyuewin <849807784@qq.com>
2019-01-19 22:17:06 +0800

    2019.01.19

--- C语言/递归非递归实现n的阶乘/递归非递归实现n的阶乘/main.c
+++ C语言/递归非递归实现n的阶乘/递归非递归实现n的阶乘/main.c
@@ -2,22 +2,40 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+int recursion(int n)
+{
+	if (n == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return recursion(n - 1) * n;
+	}
+}
+
 int factorial(int n)
 {
 	int i = 0;
-	for (i = 0; i < n; i++)
+	int sum = 1;
+	for (i = 1; i <= n; i++)
 	{
-		int sum = 
+		sum *= i;
 	}
+	return sum;
 }
+
 int main()
 {
 	int num = 0;
 	int i = 0;
-	int sum = 1;
 	printf("请输入一个整数：\n");
 	scanf("%d", &num);
-	
-system("pause");
+	int ret_1 = factorial(num);
+	int ret_2 = recursion(num);
+	printf("%d\n", ret_1);
+	printf("%d\n", ret_2);
+    system("pause");
 	return 0;
 }
