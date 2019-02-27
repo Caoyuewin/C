ffe324956b2087df0ba695bc299e9cc9e9ac3c98
Caoyuewin <849807784@qq.com>
2019-02-27 22:49:05 +0800

    2019.02.27

---
+++ C语言/反转字符串/反转字符串/main.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+
+//有一个字符数组的内容为:"student a am i",
+	//请你将数组的内容改为"i am a student".
+	//要求：
+	//不能使用库函数。
+	//只能开辟有限个空间（空间个数和字符串的长度无关）。
+
+void* reverse_str(char* str)
+{
+
+}
+
+
+int main()
+{
+	char arr[] = "student a am i";
+	reverse_str(arr);
+	system("pause");
+	return 0;
+}
