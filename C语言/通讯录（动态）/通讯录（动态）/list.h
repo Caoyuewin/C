ac01a5c406cf346090a11b330cf299fd6a2517ca
Caoyuewin <849807784@qq.com>
2019-03-08 15:18:14 +0800

    2019.03.08

---
+++ C语言/通讯录（动态）/通讯录（动态）/list.h
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#ifndef _LIST_H_
+#define _LIST_H_
+#include<stdio.h>
+#include<stdlib.h>
+#include<memory.h>
+#include<string.h>
+
+#define SIZE 1
+
+typedef struct Addresslist {
+	char name[1024];
+	char phone[1024];
+}Addresslist;//模块编程时如果多模块需要使用结构体就在头文件定义结构体
+
+typedef struct Book {
+	Addresslist num[SIZE];
+	unsigned int count;
+}Book;
+
+
+int menu();
+void InitList(Book* plist);//初始化通讯录
+void Add(Book* plist);//添加联系人
+void PrintAll(Book* plist);//打印所有联系人
+void Delete(Book* plist);//删除联系人
+void Find(Book* plist);//查找联系人
+void Amend(Book* plist);//修改联系人
+void CleanAll(Book* plist);//清除所有联系人
+Book* Capacity(Book* plist);//电话本扩容
+void Free();//释放动态开辟的空间
+
+
+
+
+
+#endif // !1
