df84bb6beb05eadfd4f8973111045b34dbba13b9
Caoyuewin <849807784@qq.com>
2019-03-20 08:39:52 +0800

    2019.03.19

--- C语言/链表/链表/test.c
+++ C语言/链表/链表/test.c
@@ -1,23 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
-#include<malloc.h>
-#include<stdlib.h>
-#include<assert.h>
+#include"test.h"
 
-typedef int SLDatatype;
-typedef struct ListNode {
-	SLDatatype value;
-	struct ListNode* next;
-}Node;
-
-typedef struct SList {
-	
-	SLDatatype value;
-	Node* first;
-}SL;
+//Í·²å
+void SListPushhead(SL* s, SLDatatype v) {
+	assert(s);
+	Node* node = malloc(sizeof(Node));
+	node->value = v;
+	node->next = s->first;
+	s->first = node;
+}
 
 //Î²²å
 void SListPushBack(SL* s, SLDatatype v) {
-	
-	Node* node = malloc()
+	assert(s);
+	assert(s->first);
+	Node* node = malloc(sizeof(Node));
+	node->value = v;
+	node->next = NULL;
+	if (s->first = NULL)
+	{
+		s->first = node;
+		return;
+	}
+	Node* current = s->first;
+	while (current->next != NULL) {
+		current = current->next;
+	}
+	current->next = node;
+
+}
+
+//Í·É¾
+void SListPophead(SL* s) {
+	assert(s);
+	assert(s->first);
+	Node* next = s->first->next;
+	free(s->first);
+	s->first = next;
 }
